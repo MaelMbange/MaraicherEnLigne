253f0b9b6ab544f7f2df1226492058ab5c5b3b4d
MaelMbange <maelmbange21@gmail.com>
2023-11-01 12:33:11 +0100

    :refactor:Using maven for server

--- JavaServeur/src/Main.java
+++
@@ -1,9 +0,0 @@
-import rti.serveur.*;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        OnPoolMainServerThread a;
-    }
-}
